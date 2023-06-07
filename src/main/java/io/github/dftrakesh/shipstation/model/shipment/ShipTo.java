015c628f01c11c89b1eb88a8e2a3097dc87c4349
krutik <krutik.khunt@eavira.com>
2023-06-07 11:36:35 +0530

    shipstation shipment

--- src/main/java/io/github/dftrakesh/shipstation/model/shipment/ShipTo.java
+++ src/main/java/io/github/dftrakesh/shipstation/model/shipment/ShipTo.java
@@ -1,10 +1,12 @@
 
 package io.github.dftrakesh.shipstation.model.shipment;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ShipTo {
 
     private String name;
